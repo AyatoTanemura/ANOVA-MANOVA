f72c4c82c37ee65fef640111a497308d9a3a7755
Ayato Tanemura <1998ayatotanemura@gmail.com>
2022-04-04 18:18:56 +1200

    add outlier detection in q1

--- Rcode/Assignment1.r
+++ Rcode/Assignment1.r
@@ -27,31 +27,58 @@ str(df.weight)
 # Viewing Data ----
 weightdiff <- df.weight$pre.weight - df.weight$weight6weeks
 
-ggplot(df.weight, aes(gender, weightdiff))+geom_boxplot()
+boxplot(weightdiff ~ gender, data = df.weight)
+boxplot(weightdiff ~ Diet, data = df.weight)
+boxplot(weightdiff ~ Diet*gender, data = df.weight)
 
-ggplot(df.weight, aes(Diet, weightdiff))+geom_boxplot()
-
+#ggplot(df.weight, aes(gender, weightdiff))+geom_boxplot()
+#ggplot(df.weight, aes(Diet, weightdiff))+geom_boxplot()
 ggplot(data = df.weight)+geom_boxplot(aes(x = Diet, y = weightdiff)) + facet_wrap(~gender)
 
-#KS test
+# Nomality chack
+##KS test
 lillie.test(weightdiff)
 lillie.test(weightdiff[df.weight$gender == 'female'])
 lillie.test(weightdiff[df.weight$gender == 'male'])
-#SW test 
+##SW test 
 shapiro.test(weightdiff)
+
+# Outlier detection
+
+outlier.weight <- boxplot(weightdiff ~ Diet*gender, data = df.weight)$out
+outlier.weight
+
+ol.weight.1.f <- df.weight[df.weight$Diet == 1 & df.weight$gender == "female" & weightdiff == 8.5,]
+ol.weight.1.m <- df.weight[df.weight$Diet == 1 & df.weight$gender == "male" & weightdiff == 9.0,]
+ol.weight.2.m <- df.weight[df.weight$Diet == 2 & df.weight$gender == "male" & weightdiff < 0,]
+ol.weight.3.f <- df.weight[df.weight$Diet == 3 & df.weight$gender == "female" & weightdiff < 1,]
+
+ol.weight.all <- rbind(ol.weight.1.f, ol.weight.1.m, ol.weight.2.m, ol.weight.3.f)
+
+weight.NoOut <- df.weight[-which(df.weight$Person %in% ol.weight.all$Person),]
+nrow(df.weight) - nrow(weight.NoOut)
+
+weightdiff.NoOut = weight.NoOut$pre.weight - weight.NoOut$weight6weeks
+
+# Homogenity check----
+
+leveneTest(weightdiff.NoOut ~ gender*Diet, data = weight.NoOut)
 
 #Two-way ANOVA ----
 #Assumption Check
 
-fit.weight <- aov(weightdiff~gender*Diet, data = df.weight)
+fit.weight <- aov(weightdiff.NoOut~gender*Diet, data = weight.NoOut)
 summary(fit.weight)
 
 #Tukey
 TukeyHSD(fit.weight)
 
+#Tamhane
+summary(T2 <- tamhaneT2Test(weightdiff.NoOut, as.factor(weight.NoOut$gender) : as.factor(weight.NoOut$Diet)))
+
 # Interaction plot
 
-interaction.plot(df.weight$Diet, df.edit$gender, weightdiff, type="b", col=c(1:3), 
+interaction.plot(weight.NoOut$Diet, weight.NoOut$gender, weightdiff.NoOut, type="b", col=c(1:3), 
                  leg.bty="o", leg.bg="beige", lwd=2, pch=c(18,24,22),	
                  xlab="Diet type", 
                  ylab="estimated marginal means", 
